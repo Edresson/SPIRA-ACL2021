bug fix in accuracy calculation and add spiraconvV2 model

models/spiraconv.py
@@ -1,14 +1,14 @@
 """
-SpiraConv model
+SpiraConv models
 """
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from utils.generic_utils import Mish
 
-class SpiraConv(nn.Module):
+class SpiraConvV2(nn.Module):
     def __init__(self, config):
-        super(SpiraConv, self).__init__()
+        super(SpiraConvV2, self).__init__()
         self.config = config
         self.audio = self.config['audio']
         self.padding_with_max_lenght = self.config.dataset['padding_with_max_lenght']
@@ -25,18 +25,23 @@ class SpiraConv(nn.Module):
             raise ValueError('Feature %s Dont is supported'%self.config.audio['feature'])
         convs = [
             # cnn1
-            nn.Conv2d(1, 32, kernel_size=(7,1), dilation=(2, 1)),
-            nn.BatchNorm2d(32), Mish(), nn.MaxPool2d(kernel_size=(2,1)),
+            nn.Dropout(p=0.5), nn.Conv2d(1, 64, kernel_size=(7,1), dilation=(2, 1)),
+            nn.BatchNorm2d(64), Mish(), nn.MaxPool2d(kernel_size=(2,1)), nn.Dropout(p=0.7), 
+            
             # cnn2
-            nn.Conv2d(32, 16, kernel_size=(5, 1), dilation=(2, 1)),
-            nn.BatchNorm2d(16), Mish(),  nn.MaxPool2d(kernel_size=(2,1)),
+            nn.Conv2d(64, 32, kernel_size=(5, 1), dilation=(2, 1)),
+            nn.BatchNorm2d(32), Mish(),  nn.MaxPool2d(kernel_size=(2,1)), nn.Dropout(p=0.7), 
 
             # cnn3
-            nn.Conv2d(16, 8, kernel_size=(3, 1), dilation=(2, 1)), 
-            nn.BatchNorm2d(8), Mish(),  nn.MaxPool2d(kernel_size=(2,1)),
+            nn.Conv2d(32, 16, kernel_size=(3, 1), dilation=(2, 1)), 
+            nn.BatchNorm2d(16), Mish(),  nn.MaxPool2d(kernel_size=(2,1)), nn.Dropout(p=0.7), 
             # cnn4
+            nn.Conv2d(16, 8, kernel_size=(2, 1), dilation=(2, 1)), 
+            nn.BatchNorm2d(8), Mish(), nn.Dropout(p=0.7),
+             
+            # cnn 5 
             nn.Conv2d(8, 4, kernel_size=(2, 1), dilation=(1, 1)), 
-            nn.BatchNorm2d(4), Mish()]
+            nn.BatchNorm2d(4), Mish(), nn.Dropout(p=0.7)]
 
         self.conv = nn.Sequential(*convs)
 
@@ -57,6 +62,7 @@ class SpiraConv(nn.Module):
             self.fc1 = nn.Linear(4*self.conv(inp).shape[-1], self.config.model['fc1_dim'])
         self.mish = Mish()
         self.fc2 = nn.Linear(self.config.model['fc1_dim'], self.config.model['fc2_dim'])
+        self.dropout = nn.Dropout(p=0.7)
 
     def forward(self, x):
         # x: [B, T, num_feature]
@@ -81,6 +87,89 @@ class SpiraConv(nn.Module):
         x = self.fc1(x) # x: [B, T, fc2_dim]
         #print(x.shape)
         x = self.mish(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = torch.sigmoid(x)
+        #print(x.shape)
+        return x
+
+class SpiraConvV1(nn.Module):
+    def __init__(self, config):
+        super(SpiraConvV1, self).__init__()
+        self.config = config
+        self.audio = self.config['audio']
+        self.padding_with_max_lenght = self.config.dataset['padding_with_max_lenght']
+        self.max_seq_len = self.config.dataset['max_seq_len']
+        print("Model Inicialized With Feature %s "%self.config.audio['feature'])
+        if self.config.audio['feature'] == 'spectrogram':
+            self.num_feature = self.config.audio['num_freq']
+        elif self.config.audio['feature'] == 'melspectrogram':
+            self.num_feature = self.config.audio['num_mels']
+        elif self.config.audio['feature'] == 'mfcc':
+            self.num_feature = self.config.audio['num_mfcc']
+        else:
+            self.num_feature = None
+            raise ValueError('Feature %s Dont is supported'%self.config.audio['feature'])
+        convs = [
+            # cnn1
+            nn.Conv2d(1, 32, kernel_size=(7,1), dilation=(2, 1)),
+            nn.BatchNorm2d(32), Mish(), nn.MaxPool2d(kernel_size=(2,1)), nn.Dropout(p=0.7), 
+            
+            # cnn2
+            nn.Conv2d(32, 16, kernel_size=(5, 1), dilation=(2, 1)),
+            nn.BatchNorm2d(16), Mish(),  nn.MaxPool2d(kernel_size=(2,1)), nn.Dropout(p=0.7), 
+
+            # cnn3
+            nn.Conv2d(16, 8, kernel_size=(3, 1), dilation=(2, 1)), 
+            nn.BatchNorm2d(8), Mish(),  nn.MaxPool2d(kernel_size=(2,1)), nn.Dropout(p=0.7), 
+            # cnn4
+            nn.Conv2d(8, 4, kernel_size=(2, 1), dilation=(1, 1)), 
+            nn.BatchNorm2d(4), Mish(), nn.Dropout(p=0.7)]
+
+        self.conv = nn.Sequential(*convs)
+
+        if self.padding_with_max_lenght:
+            # its very useful because if you change the convlutional arquiture the model calculate its, and you dont need change this :)
+            # I prefer aactivate the network in toy example because is more easy than calculate the conv output
+            # get zeros input
+            inp = torch.zeros(1, 1, self.max_seq_len, self.num_feature)
+            # get out shape
+            toy_activation_shape = self.conv(inp).shape
+            # set fully connected input dim 
+            fc1_input_dim = toy_activation_shape[1]*toy_activation_shape[2]*toy_activation_shape[3]
+            self.fc1 = nn.Linear(fc1_input_dim, self.config.model['fc1_dim'])
+        else:
+            # dinamic calculation num_feature, its useful if you use maxpooling or other pooling in feature dim, and this model dont break
+            inp = torch.zeros(1, 1, 500 ,self.num_feature)
+            # get out shape 
+            self.fc1 = nn.Linear(4*self.conv(inp).shape[-1], self.config.model['fc1_dim'])
+        self.mish = Mish()
+        self.fc2 = nn.Linear(self.config.model['fc1_dim'], self.config.model['fc2_dim'])
+        self.dropout = nn.Dropout(p=0.7)
+    def forward(self, x):
+        # x: [B, T, num_feature]
+        x = x.unsqueeze(1)
+        #print(x.shape)
+        # x: [B, 1, T, num_feature]
+        x = self.conv(x)
+        #print(x.shape)
+        # x: [B, n_filters, T, num_feature]
+        x = x.transpose(1, 2).contiguous()
+        # x: [B, T, n_filters, num_feature]
+        #print(x.shape)
+        if self.padding_with_max_lenght:
+             # x: [B, T*n_filters*num_feature]
+            x = x.view(x.size(0), -1)
+        else:
+             # x: [B, T, n_filters*num_feature]
+            x = x.view(x.size(0), x.size(1), -1)
+    
+       
+        #print(x.shape)
+        x = self.fc1(x) # x: [B, T, fc2_dim]
+        #print(x.shape)
+        x = self.mish(x)
+        x = self.dropout(x)
         x = self.fc2(x)
         x = torch.sigmoid(x)
         #print(x.shape)

train.py
@@ -12,7 +12,7 @@ import argparse
 from utils.generic_utils import load_config, save_config_file
 from utils.generic_utils import set_init_dict
 
-from utils.generic_utils import NoamLR
+from utils.generic_utils import NoamLR, binary_acc
 
 from utils.generic_utils import save_best_checkpoint
 
@@ -20,45 +20,43 @@ from utils.tensorboard import TensorboardWriter
 
 from utils.dataset import train_dataloader, eval_dataloader
 
-from models.spiraconv import SpiraConv
+from models.spiraconv import SpiraConvV1
 from utils.audio_processor import AudioProcessor 
 
 def validation(criterion, ap, model, c, testloader, tensorboard, step,  cuda):
     padding_with_max_lenght = c.dataset['padding_with_max_lenght']
+    model.zero_grad()
     model.eval()
-    losses = []
-    accs = []
-    for feature, target in testloader:       
-        #try:
-        if cuda:
-            feature = feature.cuda()
-            target = target.cuda()
+    loss = 0 
+    acc = 0
+    with torch.no_grad():
+        for feature, target in testloader:       
+            #try:
+            if cuda:
+                feature = feature.cuda()
+                target = target.cuda()
 
-        output = model(feature)
-        # Calculate loss
-        if not padding_with_max_lenght:
-            target = target[:, :output.shape[1],:target.shape[2]]
-        loss = criterion(output, target).item()
-        losses.append(loss)
-        # calculate accuracy
-        target = target.reshape(-1)
-        _, predicted = torch.max(output.data, 1)
-        total_elements = target.nelement() 
-        correct = predicted.eq(target.data).sum().item()
-        acc = 100 * correct / total_elements
-        accs.append(acc)
+            output = model(feature).float()
 
-    mean_loss = np.array(losses).mean()
-    mean_acc = np.array(accs).mean()
-    tensorboard.log_evaluation(mean_loss, mean_acc, step)
-    print("Validation\n Loss:", mean_loss, "Acurracy: ", mean_acc)
+            # Calculate loss
+            if not padding_with_max_lenght:
+                target = target[:, :output.shape[1],:target.shape[2]]
+            loss += criterion(output, target).item()
+
+            # calculate binnary accuracy
+            y_pred_tag = torch.round(output)
+            acc += (y_pred_tag == target).float().sum().item()
+
+        mean_acc = acc / len(testloader.dataset)
+        mean_loss = loss / len(testloader.dataset)
+    print("Validation:\n Loss:", mean_loss, "Acurracy: ", mean_acc)
     model.train()
     return mean_loss
 
 def train(args, log_dir, checkpoint_path, trainloader, testloader, tensorboard, c, model_name, ap, cuda=True):
     padding_with_max_lenght = c.dataset['padding_with_max_lenght']
-    if(model_name == 'spiraconv'):
-        model = SpiraConv(c)
+    if(model_name == 'spiraconv_v1'):
+        model = SpiraConvV1(c)
     #elif(model_name == 'voicesplit'):
     else:
         raise Exception(" The model '"+model_name+"' is not suported")
@@ -108,6 +106,7 @@ def train(args, log_dir, checkpoint_path, trainloader, testloader, tensorboard, 
 
     # define loss function
     criterion = nn.BCELoss()
+    eval_criterion = nn.BCELoss(reduction='sum')
 
     best_loss = float('inf')
 
@@ -123,7 +122,7 @@ def train(args, log_dir, checkpoint_path, trainloader, testloader, tensorboard, 
                 # Calculate loss
                 # adjust target dim
                 if not padding_with_max_lenght:
-                    target = target[:, :output.shape[1],:target.shape[2]]
+                    target = target[:, :output.shape[1],:]
                 loss = criterion(output, target)
                 optimizer.zero_grad()
                 loss.backward()
@@ -141,7 +140,7 @@ def train(args, log_dir, checkpoint_path, trainloader, testloader, tensorboard, 
                 # write loss to tensorboard
                 if step % c.train_config['summary_interval'] == 0:
                     tensorboard.log_training(loss, step)
-                    print("Write summary at step %d" % step, ' loss: ', loss)
+                    print("Write summary at step %d" % step, ' Loss: ', loss)
 
                 # save checkpoint file  and evaluate and save sample to tensorboard
                 if step % c.train_config['checkpoint_interval'] == 0:
@@ -154,12 +153,14 @@ def train(args, log_dir, checkpoint_path, trainloader, testloader, tensorboard, 
                     }, save_path)
                     print("Saved checkpoint to: %s" % save_path)
                     # run validation and save best checkpoint
-                    val_loss = validation(criterion, ap, model, c, testloader, tensorboard, step,  cuda=cuda)
+                    val_loss = validation(eval_criterion, ap, model, c, testloader, tensorboard, step,  cuda=cuda)
                     best_loss = save_best_checkpoint(log_dir, model, optimizer, c, step, val_loss, best_loss)
         
+        print('=================================================')
         print("Epoch %d End !"%epoch)
+        print('=================================================')
         # run validation and save best checkpoint at end epoch
-        val_loss = validation(criterion, ap, model, c, testloader, tensorboard, step,  cuda=cuda)
+        val_loss = validation(eval_criterion, ap, model, c, testloader, tensorboard, step,  cuda=cuda)
         best_loss = save_best_checkpoint(log_dir, model, optimizer, c, step, val_loss, best_loss)
 
 if __name__ == '__main__':

utils/dataset.py
@@ -12,14 +12,20 @@ class Dataset(Dataset):
     """
     Class for load a train and test from dataset generate by import_librispeech.py and others
     """
-    def __init__(self, c, ap, train=True, max_seq_len=None):
+    def __init__(self, c, ap, train=True, max_seq_len=None, test=False):
         # set random seed
         random.seed(c['seed'])
+        torch.manual_seed(c['seed'])
+        torch.cuda.manual_seed(c['seed'])
+        np.random.seed(c['seed'])
         self.c = c
         self.ap = ap
         self.train = train
         self.dataset_csv = c.dataset['train_csv'] if train else c.dataset['eval_csv']
         self.dataset_root = c.dataset['train_data_root_path'] if train else c.dataset['eval_data_root_path']
+        if test:
+            self.dataset_csv = c.dataset['test_csv']
+            self.dataset_root = c.dataset['test_data_root_path']
         self.noise_csv = c.dataset['noise_csv'] 
         self.noise_root = c.dataset['noise_data_root_path']
         assert os.path.isfile(self.dataset_csv),"Test or Train CSV file don't exists! Fix it in config.json"
@@ -130,6 +136,12 @@ def eval_dataloader(c, ap, max_seq_len=None):
                           collate_fn=own_collate_fn, batch_size=c.test_config['batch_size'], 
                           shuffle=False, num_workers=c.test_config['num_workers'])
 
+
+def test_dataloader(c, ap, max_seq_len=None):
+    return DataLoader(dataset=Dataset(c, ap, train=False, test=True, max_seq_len=max_seq_len),
+                          collate_fn=own_collate_fn, batch_size=c.test_config['batch_size'], 
+                          shuffle=False, num_workers=c.test_config['num_workers'])
+
 def own_collate_fn(batch):
     features = []
     targets = []
@@ -145,4 +157,5 @@ def own_collate_fn(batch):
 
     # list to tensor
     #targets = stack(targets, dim=0)
+    #features = stack(features, dim=0)
     return features, targets

utils/generic_utils.py
@@ -123,11 +123,28 @@ class NoamLR(torch.optim.lr_scheduler._LRScheduler):
             for base_lr in self.base_lrs
         ]
 
+def binary_acc(y_pred, y):
+    """Calculates model accuracy
+    
+    Arguments:
+        y_pred {torch.Tensor} -- Output of model between 0 and 1
+        y {torch.Tensor} -- labels/target values
+    
+    Returns:
+        [torch.Tensor] -- accuracy
+    """
+    y_pred_tag = torch.round(y_pred)
+    correct_results_sum = (y_pred_tag == y).float().sum()
+    n = y.nelement() 
+    acc = correct_results_sum/n
+    acc = acc * 100
+    return acc.item()
+
 
 def save_best_checkpoint(log_dir, model, optimizer, c, step, val_loss, best_loss):
     if val_loss < best_loss:
         best_loss = val_loss
-        save_path = os.path.join(log_dir, 'checkpoint_%d.pt' % step)
+        save_path = os.path.join(log_dir, 'best_checkpoint.pt')
         torch.save({
             'model': model.state_dict(),
             'optimizer': optimizer.state_dict(),